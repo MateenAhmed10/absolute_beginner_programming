02420fa344f5cc9f121eec5aa3f6053e8104a528
Mateen Shahbaz Ahmed <75861907+MateenAhmed10@users.noreply.github.com>
2023-03-03 18:43:12 +0500

    "Change of typescript"

--- typescript/1.variables.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let userName = "Mateen Shahbaz Ahemd Bhutto";
-let age = 19;
-let userId;
-userId = 23220;
-let userLoggedIn = false;
-console.log(userLoggedIn);
---
+++ typescript/Operators/index.ts
@@ -0,0 +1,4 @@
+let num1: number = 2;
+let power: number = num1 ** num1;
+console.log(power);
+export {};
